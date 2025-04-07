ca0f793211e06684ba63f8a3f748367ac3151443
shakilmonsi <107542773+shakilmonsi@users.noreply.github.com>
2025-04-07 17:40:00 +0600

    services cart add

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,17 @@
 
 import HeroSection from "../../components/Home/HeroSection/HeroSection";
 import ProjectsSlider from "../../components/Home/ProjectsSlider/ProjectsSlider";
-import Services from "../../components/Home/Services/Services";
+// import Services from "../../components/Home/Services/Services";
+import ServicesCart from "../../components/Home/ServicesCart/ServicesCart";
 
 
 function Home ()  {
   return (
     <div className="bg-background">
       <HeroSection></HeroSection>
-      <Services></Services>
+      <ServicesCart></ServicesCart>
+      {/* <Services></Services> */}
+
       <ProjectsSlider></ProjectsSlider>
     
     </div>
