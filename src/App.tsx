0a028412a7d790882f7d4916f0aa6c2a5782bbd9
Jahid-Hasan-Jony <jonyhossain5588@gmail.com>
2025-04-10 18:39:06 +0600

    added authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { RouterProvider } from "react-router";
 import { AppRoutes } from "./app-routes/app-routes";
 import Loading from "./components/Loading/Loading";
+import AuthProvider from "./context/AuthProvider";
 import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <>
       <Suspense fallback={<Loading></Loading>}>
         <ThemeProvider>
-          <RouterProvider router={AppRoutes} />
+          <AuthProvider>
+            <RouterProvider router={AppRoutes} />
+          </AuthProvider>
         </ThemeProvider>
       </Suspense>
     </>
