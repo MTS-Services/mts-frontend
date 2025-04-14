a8a1288c76a245669809b2ea545fb6310e0d3569
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-04-14 13:00:49 +0600

    resolve complete

--- src/pages/Auth/LoginForm.tsx
+++ src/pages/Auth/LoginForm.tsx
@@ -40,7 +40,8 @@ const LoginForm: React.FC = () => {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background font-primary relative">
+    <div className='flex items-center justify-center min-h-screen bg-background font-primary relative w-full px-4 sm:px-6 lg:px-8'>
+
       {/* Main Form Container */}
       <div className="w-full max-w-4xl p-16 space-y-8 bg-background border-2 border-gray-400 shadow-xl rounded-xl flex flex-col md:flex-row space-x-12">
         {/* Left Column: Welcome Text */}
@@ -109,10 +110,10 @@ const LoginForm: React.FC = () => {
             </div>
 
             {/* Sign In Button */}
-            <div className="flex justify-center">
+             <div className='flex justify-center'>
               <button
-                className="flex items-center relative py-2 px-30 text-background text-base font-bold rounded-full overflow-hidden bg-primary transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-800 before:to-blue-300 before:transition-all before:duration-800 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
-                type="submit"
+                className='flex items-center relative py-2 px-6 sm:px-8 md:px-10 lg:px-12 text-background text-base sm:text-lg font-bold rounded-full overflow-hidden bg-primary transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-800 before:to-blue-300 before:transition-all before:duration-800 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0'
+                type='submit'
               >
                 Sign In
               </button>
