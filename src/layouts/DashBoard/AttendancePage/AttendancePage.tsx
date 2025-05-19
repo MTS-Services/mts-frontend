2610fc574a33f029d23be5d6fe2bcad1fec249ac
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-05-19 16:10:56 +0600

    update UI design and Fetch APIs!!!!

--- src/layouts/DashBoard/AttendancePage/AttendancePage.tsx
+++ src/layouts/DashBoard/AttendancePage/AttendancePage.tsx
@@ -4,7 +4,6 @@ import Attendacne from "../../../components/Attendance/Attendance";
 const AttendacnePage = () => {
   return (
     <div>
-      <h1>This is Home Page</h1>
       <Attendacne />
     </div>
   );
