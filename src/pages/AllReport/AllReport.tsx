4a6974e826506f45eb90cafdf9f94e93ee0dfdbc
Jahid-Hasan-Jony <jonyhossain5588@gmail.com>
2025-05-21 17:35:16 +0600

    finalize

--- src/pages/AllReport/AllReport.tsx
+++ src/pages/AllReport/AllReport.tsx
@@ -2,37 +2,304 @@ import Loading from "../../components/Loading/Loading";
 import { useFetchData } from "../../hooks/useFetchData";
 
 function AllReport() {
-  const { data, loading } = useFetchData(
-    "https://mtsbackend20-production.up.railway.app/reports/all",
+  // all
+  const { data } = useFetchData(
+    "https://mtsbackend20-production.up.railway.app/api/profile/reports/all",
   );
 
-  if (loading) return <Loading />;
+  // Project Delivery Reports
+  //   const { data: projectDelivery, loading: projectDeliveryLoading } =
+  //     useFetchData(
+  //       "https://mtsbackend20-production.up.railway.app/api/profile/projects/delivered/today",
+  //     );
+
+  //   const { data: projectDeliveryMonth, loading: projectDeliveryMonthLoading } =
+  //     useFetchData(
+  //       "https://mtsbackend20-production.up.railway.app/api/profile/projects/delivered/month",
+  //     );
+
+  //   // Project Order Reports
+  //   const { data: projectOrder, loading: projectOrderLoading } = useFetchData(
+  //     "https://mtsbackend20-production.up.railway.app/api/profile/projects/ordered/today",
+  //   );
+  //   const { data: projectOrderMonth, loading: projectOrderMonthLoading } =
+  //     useFetchData(
+  //       "https://mtsbackend20-production.up.railway.app/api/profile/projects/ordered/month",
+  //     );
+
+  //   // Project Cancellation Reports
+  //   const { data: projectCancel, loading: projectCancelLoading } = useFetchData(
+  //     "https://mtsbackend20-production.up.railway.app/api/profile/projects/cancelled/month",
+  //   );
+
+  //   // Promotion Cost Reports
+  //   const { data: promotionReport, loading: promotionReportLoadin } =
+  //     useFetchData(
+  //       "https://mtsbackend20-production.up.railway.app/api/profile/promotions/today",
+  //     );
+
+  //   const { data: promotionReportMonth, loading: promotionReportMonthLoadin } =
+  //     useFetchData(
+  //       "https://mtsbackend20-production.up.railway.app/api/profile/promotions/month",
+  //     );
+
+  //   // Special Order Reports
+  //   const { data: specialOrder, loading: specialOrderLoading } = useFetchData(
+  //     "https://mtsbackend20-production.up.railway.app/api/profile/special-orders/today",
+  //   );
+  //   const { data: specialOrderMonth, loading: specialOrderMonthLoading } =
+  //     useFetchData(
+  //       "https://mtsbackend20-production.up.railway.app/api/profile/special-orders/month",
+  //     );
+
+  //   // Operational Performance Reports
+  //   const { data: operationPerformance, loading: operationPerformanceLoading } =
+  //     useFetchData(
+  //       "https://mtsbackend20-production.up.railway.app/api/profile/operational-performance",
+  //     );
+
+  //   // Sales Performance Reports
+  //   const { data: salesPerformance, loading: salesPerformanceLoading } =
+  //     useFetchData(
+  //       "https://mtsbackend20-production.up.railway.app/api/profile/sales-performance",
+  //     );
+
+  //   if (
+  //     projectDeliveryLoading ||
+  //     projectDeliveryMonthLoading ||
+  //     projectOrderLoading ||
+  //     projectOrderMonthLoading ||
+  //     projectCancelLoading ||
+  //     promotionReportLoadin ||
+  //     promotionReportMonthLoadin ||
+  //     specialOrderLoading ||
+  //     specialOrderMonthLoading ||
+  //     operationPerformanceLoading ||
+  //     salesPerformanceLoading
+  //   )
+  //     return <Loading />;
+
+  if (!data) return <Loading />;
+
+  const {
+    operationalPerformance,
+    promotionCosts,
+    salesPerformance,
+    specialOrderStats,
+    todaysDeliveries,
+    todaysOrders,
+    totalMonthlyCancellations,
+    totalMonthlyDeliveries,
+    totalMonthlyOrders,
+  } = data;
+
+  const monthlyOperationAchive =
+    operationalPerformance.achievements.this_month.total_achievement;
+  const dailyOperationAchive =
+    operationalPerformance.achievements.today.total_achievement;
+  const monthlyPromotionCost = promotionCosts?.this_month_promotion.total_cost;
+  const dailyPromotionCost = promotionCosts?.today_promotion.total_cost;
+  const monthlySpecialOrderCost =
+    specialOrderStats?.this_month_special_order.total_cost;
+  const dailySpecialOrderCost =
+    specialOrderStats?.today_special_order.total_cost;
+
+  const total_Monthly_cost =
+    parseFloat(monthlyPromotionCost) + parseFloat(monthlySpecialOrderCost);
+  const total_daily_cost =
+    parseFloat(dailyPromotionCost) + parseFloat(dailySpecialOrderCost);
+
+  const salesTargetThisMonth =
+    salesPerformance?.targets?.this_month?.total_member_target_sum;
+  const salesTargetToday =
+    salesPerformance?.targets?.today?.total_member_target_sum;
+
+  const salesAchivementThisMonth =
+    salesPerformance?.achievements?.this_month?.total_achievement;
+  const salesAchivementToday =
+    salesPerformance?.achievements?.today?.total_achievement;
 
   console.log(data);
 
   return (
-    <>
-      <table className="">
-        <thead>
-          <tr>
-            <td>Promotion Cost</td>
-            <td>Special Order Cost</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <p>{}</p>
-              <p>{}</p>
-            </td>
-            <td>
-              <p>{}</p>
-              <p>{}</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+    <section className="font-primary">
+      <div className="py-5">
+        <h2 className="pb-3 text-3xl">Main Factors</h2>
+        <table className="border-primary text-accent font-primary text-md w-full table-auto border text-center">
+          <thead className="bg-primary text-white">
+            <tr>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                +/- ( TE-TC )
+              </th>
+
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Total Earn
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Total Cost
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Promotion Cost
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Special Order Cost
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="odd:bg-secondary even:bg-background text-accent">
+              <td>
+                <p className="px-4 py-2">
+                  {parseFloat(monthlyOperationAchive) - total_Monthly_cost}
+                </p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {parseFloat(dailyOperationAchive) - total_daily_cost}
+                </p>
+              </td>
+
+              <td className="border-primary border">
+                <p className="px-4 py-2">{monthlyOperationAchive}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {dailyOperationAchive}
+                </p>
+              </td>
+              <td className="border-primary border">
+                <p className="px-4 py-2">{total_Monthly_cost}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {total_daily_cost}
+                </p>
+              </td>
+              <td className="border-primary border">
+                <p className="px-4 py-2">{monthlyPromotionCost}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {dailyPromotionCost}
+                </p>
+              </td>
+              <td className="border-primary border">
+                <p className="px-4 py-2">{monthlySpecialOrderCost}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {dailySpecialOrderCost}
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="py-5">
+        <h2 className="pb-3 text-3xl">Sales Factors</h2>
+        <table className="border-primary text-accent font-primary text-md w-full table-auto border text-center">
+          <thead className="bg-primary text-white">
+            <tr>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                +/- ( T-A )
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Target
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Achived
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Total Project
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="odd:bg-secondary even:bg-background text-accent">
+              <td>
+                <p className="px-4 py-2">
+                  {parseFloat(salesTargetThisMonth) -
+                    parseFloat(salesAchivementThisMonth)}
+                </p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {parseFloat(salesTargetToday) -
+                    parseFloat(salesAchivementToday)}
+                </p>
+              </td>
+
+              <td className="border-primary border">
+                <p className="px-4 py-2">{salesTargetThisMonth}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {salesTargetToday}
+                </p>
+              </td>
+              <td className="border-primary border">
+                <p className="px-4 py-2">{salesAchivementThisMonth}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {salesAchivementToday}
+                </p>
+              </td>
+              <td className="border-primary border">
+                <p className="px-4 py-2">{totalMonthlyOrders?.count}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {todaysOrders?.count}
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="py-5">
+        <h2 className="pb-3 text-3xl">Operation Factors</h2>
+        <table className="border-primary text-accent font-primary text-md w-full table-auto border text-center">
+          <thead className="bg-primary text-white">
+            <tr>
+              <th className="border border-white px-4 py-2 text-[18px]">+/-</th>
+
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Target
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Achived
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Cancelled
+              </th>
+              <th className="border border-white px-4 py-2 text-[18px]">
+                Special Order Cost
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="odd:bg-secondary even:bg-background text-accent">
+              <td>
+                <p className="px-4 py-2">
+                  {parseFloat(monthlyOperationAchive) - total_Monthly_cost}
+                </p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {parseFloat(dailyOperationAchive) - total_daily_cost}
+                </p>
+              </td>
+
+              <td className="border-primary border">
+                <p className="px-4 py-2">{monthlyOperationAchive}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {dailyOperationAchive}
+                </p>
+              </td>
+              <td className="border-primary border">
+                <p className="px-4 py-2">{total_Monthly_cost}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {total_daily_cost}
+                </p>
+              </td>
+              <td className="border-primary border">
+                <p className="px-4 py-2">{monthlyPromotionCost}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {dailyPromotionCost}
+                </p>
+              </td>
+              <td className="border-primary border">
+                <p className="px-4 py-2">{monthlySpecialOrderCost}</p>
+                <p className="border-primary/20 border-t-1 px-4 py-2">
+                  {dailySpecialOrderCost}
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
 }
 
